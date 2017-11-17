04af017780013ba383931eb5b42f9e610f69cc90
Johannes Snyman <jsnyman@trustwave.com>
2017-11-17 09:35:33 +0200

    JS: Latest changes

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,11 +1,10 @@
 package burp;
 
 import javax.swing.*;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * @author J Snyman
@@ -13,22 +12,11 @@ import java.util.List;
  */
 public class BurpExtender implements IBurpExtender, IContextMenuFactory {
 
+
+    private final static float VERSION = 1.2f;
+
     private static IBurpExtenderCallbacks callbacks;
     private static IExtensionHelpers helpers;
-
-    @Override
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks iBurpExtenderCallbacks) {
-        callbacks = iBurpExtenderCallbacks;
-        callbacks.setExtensionName("Airachnid");
-        callbacks.registerContextMenuFactory(this);
-        helpers = iBurpExtenderCallbacks.getHelpers();
-
-        callbacks.printOutput("Airachnid Version 1.0");
-        callbacks.printOutput("Johan Snyman <jsnyman@trustwave.com>");
-        callbacks.printOutput("");
-        callbacks.printOutput("Check here for output from this extension.");
-        callbacks.printOutput("");
-    }
 
     protected static IExtensionHelpers getHelpers() {
         return helpers;
@@ -38,8 +26,36 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory {
         return callbacks;
     }
 
-    protected static void printStdOut(String str) {
+    protected static void print(String str) {
         callbacks.printOutput(str);
+    }
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks iBurpExtenderCallbacks) {
+        callbacks = iBurpExtenderCallbacks;
+        callbacks.setExtensionName("Web Cache Deception Scanner");
+        callbacks.registerContextMenuFactory(this);
+        callbacks.registerScannerCheck(new WebCacheScannerCheck());
+        callbacks.printOutput("Web Cache Deception Scanner Version " + VERSION);
+        callbacks.printOutput("Johan Snyman <jsnyman@trustwave.com>");
+
+        helpers = iBurpExtenderCallbacks.getHelpers();
+    }
+
+    private List<IScanIssue> runScannerForRequest(IHttpRequestResponse iHttpRequestResponse) {
+        ExecutorService service = Executors.newFixedThreadPool(1);
+        Future<List<IScanIssue>> task = service.submit(new ScannerThread(iHttpRequestResponse));
+        List<IScanIssue> result = null;
+
+        try {
+            result = task.get();
+        } catch(final InterruptedException ex) {
+            ex.printStackTrace();
+        } catch(final ExecutionException ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
     }
 
     /**
@@ -56,7 +72,7 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory {
                 IContextMenuInvocation.CONTEXT_TARGET_SITE_MAP_TREE == iContextMenuInvocation.getInvocationContext() ||
                 IContextMenuInvocation.CONTEXT_PROXY_HISTORY == iContextMenuInvocation.getInvocationContext()) {
             IHttpRequestResponse[] arr = iContextMenuInvocation.getSelectedMessages();
-            JMenuItem item = new JMenuItem("Airachnid Web Cache Test");
+            JMenuItem item = new JMenuItem("Web Cache Deception Test");
             MenuItemListener mil = new MenuItemListener(arr);
             item.addActionListener(mil);
             items.add(item);
@@ -78,8 +94,71 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory {
         @Override
         public void actionPerformed(ActionEvent e) {
             for (IHttpRequestResponse message : arr) {
-                RequestSender rs = new RequestSender(message);
+                List<IScanIssue> result = runScannerForRequest(message);
+
+                if (result != null) {
+                    callbacks.addScanIssue(result.get(0));
+                }
             }
+        }
+    }
+
+    class WebCacheScannerCheck implements IScannerCheck {
+
+        @Override
+        public List<IScanIssue> doPassiveScan(IHttpRequestResponse iHttpRequestResponse) {
+            // bugger all done here, no passive scanning
+            return null;
+        }
+
+        @Override
+        public List<IScanIssue> doActiveScan(IHttpRequestResponse iHttpRequestResponse, IScannerInsertionPoint iScannerInsertionPoint) {
+            return runScannerForRequest(iHttpRequestResponse);
+        }
+
+        @Override
+        public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue) {
+            if (existingIssue.getUrl().equals(newIssue.getUrl())) return -1;
+            else return 0;
+        }
+    }
+
+    class ScannerThread implements Callable<List<IScanIssue>> {
+
+        private List<IScanIssue> result = null;
+        private IHttpRequestResponse reqRes;
+
+        ScannerThread(IHttpRequestResponse reqRes) {
+            this.reqRes = reqRes;
+        }
+
+        public List<IScanIssue> call() {
+            // Test One: Does appending to the URL return a similar response.
+            if (RequestSender.initialTest(reqRes)) {
+                Set<String> fileTypesCached = new HashSet<>();
+
+                // Test two: Check if caching is done by file type
+                for (String ext : RequestSender.INITIAL_TEST_EXTENSIONS) {
+                    if (RequestSender.getFileTypeCached(reqRes, ext)) {
+                        fileTypesCached.add(ext);
+                    }
+                }
+
+                if (fileTypesCached.size() != 0) {
+                    for (String ext : RequestSender.OTHER_TEST_EXTENSIONS) {
+                        if (RequestSender.getFileTypeCached(reqRes, ext)) {
+                            fileTypesCached.add(ext);
+                        }
+                    }
+
+                    WebCacheIssue issue = new WebCacheIssue(reqRes);
+                    issue.setVulnerableExtensions(fileTypesCached);
+
+                    return Arrays.asList(issue);
+                }
+            }
+
+            return result;
         }
     }
 }
--- src/main/java/burp/RequestSender.java
+++ src/main/java/burp/RequestSender.java
@@ -1,96 +1,37 @@
 package burp;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class RequestSender implements Runnable {
+class RequestSender {
 
-    private final static int FUZZY_THRESHOLD = 5;
-    private final static double JARO_THRESHOLD = 0.8;
-    private final static int LEVENSHTEIN_THRESHOLD = 200;
+//    private final static int      FUZZY_THRESHOLD = 5;
+    private final static double   JARO_THRESHOLD = 0.8;
+    private final static int      LEVENSHTEIN_THRESHOLD = 200;
 
-    private final static String[] INITIAL_TEST_EXTENSIONS = {"css", "jpg", "js"};
-    private final static String[] OTHER_TEST_EXTENSIONS = {"html", "htm", "gif", "png", "cgi", "pl", "java", "class",
+    private final static String WORD = "test";
+
+    protected final static String[] INITIAL_TEST_EXTENSIONS = {"css", "jpg", "js"};
+    protected final static String[] OTHER_TEST_EXTENSIONS = {"html", "htm", "gif", "png", "cgi", "pl", "java", "class",
             "php", "php3", "shtm", "shtml", "cfm", "cfml", "doc", "log", "txt", "csv", "ppt", "m4a", "mid", "mp3", "flv",
             "m4v", "mov", "tif", "svg", "pdf", "xls", "sql", "bat", "exe", "jsp", "asp", "aspx", "jpeg"};
-    private final static String[] ALL_TEST_EXTENSIONS = ArrayUtils.addAll(INITIAL_TEST_EXTENSIONS, OTHER_TEST_EXTENSIONS);
 
-    private static IHttpRequestResponse message;
+    private static byte[] orgResponse;
 
-    public RequestSender(IHttpRequestResponse message) {
-        this.message = message;
-        Thread t = new Thread(this);
-        t.start();
-    }
-
-    @Override
-    public void run() {
-        // Test One: Does appending to the URL return a similar response.
-        if (isURLVulnerable()) {
-            BurpExtender.getCallbacks().printOutput("URL is vulnerable, adding text gets the same response");
-            // Test two: Check if caching is done by filetype
-            Set<String> vulnExt = new HashSet<>();
-
-//            // There are two ways to tackle this:
-//            // 1. Test a sample set of extensions:
-////            for (int i = 0; i < INITIAL_TEST_EXTENSIONS.length; i++) {
-////                String test = INITIAL_TEST_EXTENSIONS[i];
-////                if (isFileTypeCached(test)) {
-////                    BurpExtender.getCallbacks().printOutput(test + " extension is vulnerable!");
-////                    vulnExt.add(test);
-////                } else {
-////                    BurpExtender.getCallbacks().printOutput("." + test + " page is not cached.");
-////                }
-////            }
-////
-////            if (!vulnExt.isEmpty()) {
-////                BurpExtender.getCallbacks().printOutput("Checking other extensions");
-////                for (int i = 0; i < OTHER_TEST_EXTENSIONS.length; i++) {
-////                    if (isFileTypeCached(OTHER_TEST_EXTENSIONS[i])) {
-////                        vulnExt.add(OTHER_TEST_EXTENSIONS[i]);
-////                    }
-////                }
-////
-////                BurpExtender.getCallbacks().printOutput("Done checking other extensions");
-////                BurpExtender.getCallbacks().printOutput("Adding issue");
-////
-////                WebCacheIssue issue = new WebCacheIssue(message);
-////                issue.setVulnerableExtensions(vulnExt);
-////
-////                BurpExtender.getCallbacks().addScanIssue(issue);
-////            }
-//
-//            // 2. Test all extensions
-//            for (int i = 0; i < ALL_TEST_EXTENSIONS.length; i++) {
-//                String test = ALL_TEST_EXTENSIONS[i];
-//                if (isFileTypeCached(test)) {
-//                    BurpExtender.getCallbacks().printOutput(test + " extension is vulnerable!");
-//                    vulnExt.add(test);
-//                } else {
-//                    BurpExtender.getCallbacks().printOutput("." + test + " page is not cached.");
-//                }
-//            }
-//
-//            if (vulnExt.size() > 0) {
-//                BurpExtender.getCallbacks().printOutput("Done testing, adding vuln");
-//                WebCacheIssue issue = new WebCacheIssue(message);
-//                issue.setVulnerableExtensions(vulnExt);
-//
-//                BurpExtender.getCallbacks().addScanIssue(issue);
-//            }
-//        } else {
-//            BurpExtender.getCallbacks().printOutput("URL is not vulnerable");
-        }
-    }
-
-    private boolean isURLVulnerable() {
+    /**
+     * The initial test checks that the response is not the same for request with and without cookies (i.e. auth is
+     * used). Then checks that appending "/test? returns the same page
+     * @param message
+     * @return
+     */
+    protected static boolean initialTest(IHttpRequestResponse message) {
         // Send the original request again
         byte[] orgRequest = buildHttpRequest(message, null, null, true);
-        byte[] orgResponse = retrieveResponseBody(message.getHttpService(), orgRequest, true);
+        orgResponse = retrieveResponseBody(message.getHttpService(), orgRequest, true);
 
         // Send an unauthenticated - to root out fp's. Unauthenticated should not be the same as original
         byte[] unAuthedRequest = buildHttpRequest(message, null, null, false);
@@ -98,30 +39,41 @@ public class RequestSender implements Runnable {
 
         // Test that the original request and an unauthenticated request do not get the same response.
         // Same here is similar, according to the thresholds set
-        if (testSimilar(new String(orgResponse), new String(unAuthedResponse))) {
-            BurpExtender.printStdOut("This request does not use authentication.");
+        boolean authed = testSimilar(new String(orgResponse), new String(unAuthedResponse));
+        if (authed) {
+            BurpExtender.print("Request not vulnerable");
             return false;
         }
 
-        // Send with /test appended
-        byte[] testRequest = buildHttpRequest(message, "test", null, true);
+        // Send with /test appended, check that everything after is ignored
+        byte[] testRequest = buildHttpRequest(message, WORD, null, true);
         byte[] testResponse = retrieveResponseBody(message.getHttpService(), testRequest, true);
 
-        return testSimilar(new String(orgResponse), new String(testResponse));
+        boolean append = testSimilar(new String(orgResponse), new String(testResponse));
+        if (!append) {
+            BurpExtender.print("Request not vulnerable.");
+        }
+        return append;
     }
 
-    private boolean isFileTypeCached(String extension) {
-        byte[] orgRequest = buildHttpRequest(message, "test", extension, true);
-        byte[] orgResponse = retrieveResponseBody(message.getHttpService(), orgRequest, true);
+    protected static boolean getFileTypeCached(IHttpRequestResponse message, String extension) {
+        // Send with extension, potentially create cached version of resource
+        byte[] extRequest = buildHttpRequest(message, WORD, extension, true);
+        byte[] extResponse = retrieveResponseBody(message.getHttpService(), extRequest, false);
 
-        // Send an unauthenticated
-        byte[] unAuthedRequest = buildHttpRequest(message, "test", extension, false);
-        byte[] unAuthedResponse = retrieveResponseBody(message.getHttpService(), unAuthedRequest, false);
+        // Send an unauthenticated, test if vulnerable
+        byte[] vulRequest = buildHttpRequest(message, WORD, extension, false);
+        byte[] vulResponse = retrieveResponseBody(message.getHttpService(), vulRequest, false);
 
-        return new String(orgResponse).equals(new String(unAuthedResponse));
+        boolean eq = testSimilar(new String(extResponse), new String(vulResponse)); //= new String(extResponse).equals(new String(vulResponse));
+        if (!eq) {
+            BurpExtender.print("Request is not vulnerable.");
+        }
+
+        return eq;
     }
 
-    private byte[] buildHttpRequest(final IHttpRequestResponse reqRes, final String additional, final String extension,
+    private static byte[] buildHttpRequest(final IHttpRequestResponse reqRes, final String additional, final String extension,
                                     boolean addCookies) {
         byte[] result;
 
@@ -130,9 +82,8 @@ public class RequestSender implements Runnable {
         List<IParameter> params = reqInfo.getParameters();
         List<String> headers = reqInfo.getHeaders();
 
+        // Create GET message
         if ("GET".equals(reqInfo.getMethod())) {
-            // Create GET message
-
             URL url = null;
             if (additional != null) {
                 try {
@@ -153,8 +104,7 @@ public class RequestSender implements Runnable {
                     }
                 }
             }
-        } else {
-            // Create POST message
+        } else { // Create POST message
             byte[] req = reqRes.getRequest();
             int len = req.length - reqInfo.getBodyOffset();
             byte[] body = new byte[len];
@@ -182,16 +132,14 @@ public class RequestSender implements Runnable {
         return result;
     }
 
-    private byte[] retrieveResponseBody(IHttpService service, byte[] request, boolean checkResponseCode) {
+    private static byte[] retrieveResponseBody(IHttpService service, byte[] request, boolean checkResponseCode) {
         byte[] result = null;
 
         IHttpRequestResponse test = BurpExtender.getCallbacks().makeHttpRequest(service, request);
         byte[] res = test.getResponse();
         IResponseInfo responseInfo = BurpExtender.getHelpers().analyzeResponse(res);
 
-        if (checkResponseCode && responseInfo.getStatusCode() != 200) {
-            BurpExtender.printStdOut("Response message is not 200 OK, ignoring");
-        } else {
+        if (checkResponseCode && responseInfo.getStatusCode() == 200) {
             int len = res.length - responseInfo.getBodyOffset();
             result = new byte[len];
             System.arraycopy(res, responseInfo.getBodyOffset(), result, 0, len);
@@ -201,7 +149,7 @@ public class RequestSender implements Runnable {
     }
 
 
-    private URL createNewURL(URL orgURL, String additional, String extension) throws MalformedURLException {
+    private static URL createNewURL(URL orgURL, String additional, String extension) throws MalformedURLException {
         String urlStr = orgURL.toExternalForm();
 
         int pos = urlStr.indexOf("?");
@@ -221,7 +169,7 @@ public class RequestSender implements Runnable {
         return new URL(path);
     }
 
-    private String createNewUrl(String url, String additional, String extension) {
+    private static String createNewUrl(String url, String additional, String extension) {
         StringBuilder result = new StringBuilder();
 
         int pos = url.indexOf("?");
@@ -247,16 +195,23 @@ public class RequestSender implements Runnable {
         return result.toString();
     }
 
-    private boolean testSimilar(String firstString, String secondString) {
-        int fuzzyDist = StringUtils.getFuzzyDistance(firstString, secondString, Locale.getDefault());
+    /**
+     * Testing if the responses of two requests are similar. This is the not the same as the same, rather there is a
+     * threshold set in the static parameters of the class.
+     * @param firstString
+     * @param secondString
+     * @return Test if similar
+     */
+    private static boolean testSimilar(String firstString, String secondString) {
+//        int fuzzyDist = StringUtils.getFuzzyDistance(firstString, secondString, Locale.getDefault());
         double jaroDist = StringUtils.getJaroWinklerDistance(firstString, secondString);
         int levenDist = StringUtils.getLevenshteinDistance(firstString, secondString);
 
-        BurpExtender.getCallbacks().printOutput("============================================");
-        BurpExtender.getCallbacks().printOutput("Fuzzy Distance:" + fuzzyDist);
-        BurpExtender.getCallbacks().printOutput("Jaro Winkler Distance:" + jaroDist);
-        BurpExtender.getCallbacks().printOutput("Levenshtein Distance:" + levenDist);
-        BurpExtender.getCallbacks().printOutput("============================================");
+//        BurpExtender.print("============================================");
+//        BurpExtender.print("Fuzzy Distance:" + fuzzyDist);
+//        BurpExtender.print("        Jaro Winkler Distance:" + jaroDist);
+//        BurpExtender.print("        Levenshtein Distance:" + levenDist);
+//        BurpExtender.print("============================================");
 
         return jaroDist >= JARO_THRESHOLD || levenDist <= LEVENSHTEIN_THRESHOLD;
     }
